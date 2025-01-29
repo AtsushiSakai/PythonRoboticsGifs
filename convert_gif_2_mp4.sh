0775c077e2b363d5f7fb1aeb1f7727719ce1ba0b
Atsushi Sakai <asakai.amsl+github@gmail.com>
2025-01-29 12:38:33 +0900

    convert all gifs to mp4

--- convert_gif_2_mp4.sh
+++ convert_gif_2_mp4.sh
@@ -16,7 +16,7 @@ dir_name=$(dirname "$input_file")
 base_name=$(basename "$input_file" .gif)
 output_file="$dir_name/$base_name.mp4"
 
-ffmpeg -stream_loop 2 -i "$input_file" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "$output_file"
+ffmpeg -stream_loop 7 -i "$input_file" -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" "$output_file"
 
 if [ $? -eq 0 ]; then
   echo "Conversion successful! MP4 saved to: $output_file"
